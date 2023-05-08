299479a5c1fe0209e5d8b316f82e18b42b1e879d
stas <point.blank.point@bk.ru>
2023-05-08 22:05:02 +0300

    t3 done

---
+++ t3/Program.cs
@@ -0,0 +1,28 @@
+﻿int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Akk(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        n = Akk(m - 1, 1);
+        return n;
+    }
+    else if (m > 0 && n > 0)
+    {
+        n = Akk(m, n - 1);
+        n = Akk(m - 1, n);
+    }
+    return n;
+}
+
+int m = InputNum("Введите число M: ");
+int n = InputNum("Введите число N: ");
+Console.WriteLine($"A ({m}, {n}) = {Akk(m, n)}");
